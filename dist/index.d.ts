f9af0c2e4f00a8063ce288483190ae534b30de7c
Troy Chaplin <troy.chaplin@gmail.com>
2026-02-28 14:48:51 -0500

    removed addon

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { Button } from './components/Button';
-export type { ButtonProps } from './components/Button';
-export { Card } from './components/Card';
-export type { CardProps } from './components/Card';
-//# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import { Button as t } from "./components/Button/Button.js";
-import { Card as f } from "./components/Card/Card.js";
-export {
-  t as Button,
-  f as Card
-};
